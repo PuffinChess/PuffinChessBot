eaeb55cc42ea6da63796c0bacd77ef7535a4430f
Milan Rasche <theslipperytrout@gmail.com>
2024-02-29 19:09:37 +0100

    adding unity to the git repo

---
+++ PuffinChessProject/Assets/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color _baseColor, _offsetColor;
+
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    public void Init(bool isOffset)
+    {
+        _spriteRenderer.color = isOffset ? _baseColor : _offsetColor;
+    }
+}
