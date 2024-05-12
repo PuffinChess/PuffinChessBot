09420bd309c83ab26d438e5eef1be2f47b5b930e
Milan Rasche <theslipperytrout@gmail.com>
2024-05-12 23:48:53 +0200

    Started setting framework for apis. Working on getting everything implemented

---
+++ StorkChessEngine/StorkEngine/ChessEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorkEngine
+{
+    public class ChessEngine
+    {
+        //Just placeholder at the time being to help with api setup and creation.
+
+        public ChessEngine() { }
+
+        public void NewGame()
+        {
+            //Start Game
+        }
+    }
+}
---
+++ StorkChessEngine/UCIApi/Controllers/ChessController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StorkEngine;
+
+namespace UCIApi.Controllers
+{
+    [ApiController]
+    [Route("api/chess")]
+    public class ChessController : Controller
+    {
+        private readonly ChessEngine _chessEngine;
+
+        public ChessController (ChessEngine chessEngine)
+        {
+            _chessEngine = chessEngine;
+        }
+
+        [HttpPost("start-game")]
+        public IActionResult StartGame()
+        {
+            _chessEngine.NewGame(); 
+            return Ok
+        }
+    }
+}
